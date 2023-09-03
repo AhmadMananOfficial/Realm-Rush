50169fe56a469055a3b4411817ddf4be908546b1
AhmadMananOfficial <ahmadmananofficial@gmail.com>
2023-09-03 02:11:05 -0700

    Lists

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+	
+	[SerializeField] List<WayPoint> Path = new List<WayPoint>();
+    
+    void Start()
+    {
+	    PrintWayPointName();
+    }
+
+    
+	 void PrintWayPointName()
+    {
+	    foreach(WayPoint waypoint in Path)
+	    {
+	    	Debug.Log(waypoint.name);
+	    }
+    }
+}
