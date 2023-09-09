6d2ead6a4493c840b01d6cbb4b2b780027213ff6
AhmadMananOfficial <ahmadmananofficial@gmail.com>
2023-09-08 20:44:21 -0700

    dfd

---
+++ Assets/PathFinding/PathFinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+	[SerializeField] Node currentSearchNode;
+	Vector2Int[] directions = {Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down};
+	GridManager gridManager; 
+    
+	void Awake()
+	{
+		gridManager = FindObjectOfType<GridManager>();
+	}
+    
+    void Start()
+    {
+        
+    }
+
+   
+}
